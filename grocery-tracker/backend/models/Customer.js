0a9fa04283a9f75cae30f405fc3a064cf90ee1fb
MANESH THAKUR <tha.rupesh456@gmail.com>
2026-03-26 19:26:18 +0530

    Delete grocery-tracker directory

--- grocery-tracker/backend/models/Customer.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const customerSchema = new mongoose.Schema({
-  name: { type: String },
-  phone: { type: String, required: true, unique: true },
-  purchaseHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Bill' }]
-}, { timestamps: true });
-
-module.exports = mongoose.model('Customer', customerSchema);
--- grocery-tracker/backend/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getStats } = require('../controllers/dashboardController');
-const { protect, adminOrAuthority } = require('../middlewares/authMiddleware');
-
-router.get('/stats', protect, adminOrAuthority, getStats);
-
-module.exports = router;
