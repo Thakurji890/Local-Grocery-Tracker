cf847d8f6ef6c443dc21da74ad4161c2e829fdb1
MANESH THAKUR <tha.rupesh456@gmail.com>
2026-03-26 19:42:43 +0530

    Delete grocery-bill-tracker directory

--- grocery-bill-tracker/backend/routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getDashboardStats } = require('../controllers/dashboardController');
-const { protect, authorize } = require('../middleware/auth');
-
-const router = express.Router();
-router.use(protect);
-router.get('/stats', getDashboardStats);
-
-module.exports = router;
